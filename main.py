97dbc90a68c0280db7d9af302445f0d569db57b1
Milad Heidari <milad.h9999@gmail.com>
2026-01-27 18:40:15 -0800

    Add: main code

---
+++ main.py
@@ -0,0 +1,87 @@
+import os
+import envs
+import wandb
+import torch
+import minari
+import joblib
+import logging
+import argparse
+import numpy as np
+from pathlib import Path
+from datetime import datetime
+from omegaconf import OmegaConf
+from minari import MinariDataset
+from sklearn.preprocessing import StandardScaler
+from dfine.memory import ReplayBuffer
+from envs.utils import collect_data
+from dfine.train import train_backbone
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="DFINE")
+    parser.add_argument("--config", type=str, help="path to the config file")
+    args = parser.parse_args()
+    
+    config = OmegaConf.load(args.config)
+    config.run_id = datetime.now().strftime("%Y%m%d_%H%M")
+
+    wandb.init(
+        project="Manifolds control",
+        name=config.run_name,
+        notes=config.notes,
+        config=OmegaConf.to_container(config, resolve=True)
+    )
+
+    # prepare logging
+    save_dir = Path(config.log_dir) / config.run_id
+    os.makedirs(save_dir, exist_ok=True)
+    OmegaConf.save(config, save_dir / "config.yaml")
+    wandb.define_metric("global_step")
+    wandb.define_metric("*", step_metric="global_step")
+    logger = logging.getLogger(__name__)
+
+    # set seed
+    np.random.seed(config.seed)
+    torch.manual_seed(config.seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(config.seed)
+
+    # create env and collect data
+    env = envs.make(config=config.env)
+    logger.info("collecting data ...")
+    collect_data(
+        env=env,
+        data_dir=save_dir / "data",
+        num_episodes=config.data.num_episodes,
+        action_repeat=config.data.action_repeat,
+    )
+    
+    # create replay buffers
+    dataset = MinariDataset(data=save_dir / "data")
+    test_size = int(len(dataset) * config.data.test_ratio)
+    train_size = len(dataset) - test_size
+    train_data, test_data = minari.split_dataset(dataset=dataset, sizes=[train_size, test_size])
+    train_buffer = ReplayBuffer.load_from_minari(dataset=train_data)
+    test_buffer = ReplayBuffer.load_from_minari(dataset=test_data)
+
+    # normalize observations
+    obs_scaler = StandardScaler()
+    obs_scaler.fit(train_buffer.ys)
+    train_buffer.ys = obs_scaler.transform(train_buffer.ys)
+    test_buffer.ys = obs_scaler.transform(test_buffer.ys)
+
+    # save scaler
+    joblib.dump(obs_scaler, save_dir / "obs_scaler.bin")
+
+    # train and save the backbone
+    logging.info("training backbone ...")
+    encoder, decoder, dynamics_model = train_backbone(
+        config=config.train.backbone,
+        train_buffer=train_buffer,
+        test_buffer=test_buffer,
+    )
+    torch.save(encoder.state_dict(), save_dir / "encoder.pth")
+    torch.save(decoder.state_dict(), save_dir / "decoder.pth")
+    torch.save(dynamics_model.state_dict(), save_dir / "dynamics_model.pth")
+    
+    wandb.finish()
